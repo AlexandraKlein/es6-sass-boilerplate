85a27bc3e197da9ae4b021c34d0f3beb118d2f82
ali.klein <alexandraeklein@yahoo.com>
2019-01-05 15:52:19 -0800

    js components

---
+++ src/js/components/buttonEffect.js
@@ -0,0 +1,21 @@
+function directionAwareButton() {
+  $('.btn')
+    .on('mouseenter', function(e) {
+      const parentOffset = $(this).offset();
+      const relX = e.pageX - parentOffset.left;
+      const relY = e.pageY - parentOffset.top;
+      $(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+      const parentOffset = $(this).offset();
+      const relX = e.pageX - parentOffset.left;
+      const relY = e.pageY - parentOffset.top;
+      $(this).find('span').css({top:relY, left:relX})
+    });
+}
+
+$(() => directionAwareButton());
+
+$($document).on('allPagesEnter', () => {
+  directionAwareButton();
+});
